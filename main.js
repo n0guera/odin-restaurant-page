4919f4271f843d82583c806f6418d555fc0e45a1
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-06-15 12:49:15 -0300

    Add createFooter function

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 (() => {
   'use strict';
+  const e = document.querySelector('#content');
   (() => {
-    const e = document.querySelector('#content'),
-      t = document.createElement('header'),
+    const t = document.createElement('header'),
       n = document.createElement('nav'),
       c = document.createElement('ul'),
       d = document.createElement('li');
     d.textContent = 'Home';
-    const l = document.createElement('li');
-    l.textContent = 'Menu';
     const o = document.createElement('li');
-    (o.textContent = 'Contact'),
+    o.textContent = 'Menu';
+    const l = document.createElement('li');
+    (l.textContent = 'Contact'),
       c.appendChild(d),
-      c.appendChild(l),
       c.appendChild(o),
+      c.appendChild(l),
       n.appendChild(c),
       t.appendChild(n),
       e.appendChild(t);
-  })();
+  })(),
+    (() => {
+      const e = document.createElement('footer'),
+        t = document.createElement('p');
+      (t.textContent = 'Made with <3 for The Odin Project curriculum'),
+        e.appendChild(t),
+        document.body.appendChild(e);
+    })();
 })();
