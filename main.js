a92fe70de19985b49208bb50eae81e5f9b3b199a
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-06-16 22:25:31 -0300

    Add main element and fix footer

--- main.js
+++ main.js
@@ -4,25 +4,36 @@
   (() => {
     const t = document.createElement('header'),
       n = document.createElement('nav'),
-      c = document.createElement('ul'),
+      o = document.createElement('ul'),
       d = document.createElement('li');
     d.textContent = 'Home';
-    const o = document.createElement('li');
-    o.textContent = 'Menu';
-    const l = document.createElement('li');
-    (l.textContent = 'Contact'),
-      c.appendChild(d),
-      c.appendChild(o),
-      c.appendChild(l),
-      n.appendChild(c),
+    const c = document.createElement('li');
+    c.textContent = 'Menu';
+    const i = document.createElement('li');
+    (i.textContent = 'Contact'),
+      o.appendChild(d),
+      o.appendChild(c),
+      o.appendChild(i),
+      n.appendChild(o),
       t.appendChild(n),
       e.appendChild(t);
   })(),
     (() => {
-      const e = document.createElement('footer'),
-        t = document.createElement('p');
-      (t.textContent = 'Made with <3 for The Odin Project curriculum'),
-        e.appendChild(t),
-        document.body.appendChild(e);
+      const t = document.createElement('main'),
+        n = document.createElement('h1');
+      n.textContent = 'An Awesome Restaurant';
+      const o = document.createElement('p');
+      (o.textContent =
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus qui dolor fugit facere. Corrupti ea sit quidem quod nemo rem aliquid voluptas ratione eos dolorem odio, cum animi! Nemo.'),
+        t.appendChild(n),
+        t.appendChild(o),
+        e.appendChild(t);
+    })(),
+    (() => {
+      const t = document.createElement('footer'),
+        n = document.createElement('p');
+      (n.textContent = 'Made with <3 for The Odin Project curriculum'),
+        t.appendChild(n),
+        e.appendChild(t);
     })();
 })();
