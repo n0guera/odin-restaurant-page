97489bf424d9b30fc2b4d1fe4704ed07ccc5b80c
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-06-19 13:36:59 -0300

    Add hours table

--- dist/main.js
+++ dist/main.js
@@ -4,17 +4,17 @@
   (() => {
     const t = document.createElement('header'),
       n = document.createElement('nav'),
-      o = document.createElement('ul'),
-      d = document.createElement('li');
-    d.textContent = 'Home';
-    const c = document.createElement('li');
-    c.textContent = 'Menu';
-    const i = document.createElement('li');
-    (i.textContent = 'Contact'),
-      o.appendChild(d),
-      o.appendChild(c),
-      o.appendChild(i),
-      n.appendChild(o),
+      d = document.createElement('ul'),
+      o = document.createElement('li');
+    o.textContent = 'Home';
+    const a = document.createElement('li');
+    a.textContent = 'Menu';
+    const m = document.createElement('li');
+    (m.textContent = 'Contact'),
+      d.appendChild(o),
+      d.appendChild(a),
+      d.appendChild(m),
+      n.appendChild(d),
       t.appendChild(n),
       e.appendChild(t);
   })(),
@@ -22,10 +22,27 @@
       const t = document.createElement('main'),
         n = document.createElement('h1');
       n.textContent = 'An Awesome Restaurant';
-      const o = document.createElement('p');
-      (o.textContent =
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus qui dolor fugit facere. Corrupti ea sit quidem quod nemo rem aliquid voluptas ratione eos dolorem odio, cum animi! Nemo.'),
+      const d = document.createElement('p');
+      d.textContent =
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus qui dolor fugit facere. Corrupti ea sit quidem quod nemo rem aliquid voluptas ratione eos dolorem odio, cum animi! Nemo.';
+      const o = document.createElement('div'),
+        a = document.createElement('h3');
+      (a.textContent = 'Hours'), o.appendChild(a);
+      const m = document.createElement('p');
+      (m.textContent = 'Monday: 6am - 6pm'), o.appendChild(m);
+      const c = document.createElement('p');
+      (c.textContent = 'Tuesday: 6am - 6pm'), o.appendChild(c);
+      const p = document.createElement('p');
+      (p.textContent = 'Wednesday: 6am - 6pm'), o.appendChild(p);
+      const l = document.createElement('p');
+      (l.textContent = 'Thursday: 6am - 10pm'), o.appendChild(l);
+      const i = document.createElement('p');
+      (i.textContent = 'Friday: 6am - 10pm'), o.appendChild(i);
+      const r = document.createElement('p');
+      (r.textContent = 'Saturday: 8am - 10pm'),
+        o.appendChild(r),
         t.appendChild(n),
+        t.appendChild(d),
         t.appendChild(o),
         e.appendChild(t);
     })(),
--- src/main-content.js
+++ src/main-content.js
@@ -9,8 +9,40 @@ const createMainContent = () => {
   const presentation = document.createElement('p');
   presentation.textContent =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus qui dolor fugit facere. Corrupti ea sit quidem quod nemo rem aliquid voluptas ratione eos dolorem odio, cum animi! Nemo.';
+
+  const hoursTable = document.createElement('div');
+
+  const hoursTitle = document.createElement('h3');
+  hoursTitle.textContent = 'Hours';
+  hoursTable.appendChild(hoursTitle);
+
+  const hoursMonday = document.createElement('p');
+  hoursMonday.textContent = 'Monday: 6am - 6pm';
+  hoursTable.appendChild(hoursMonday);
+
+  const hoursTuesday = document.createElement('p');
+  hoursTuesday.textContent = 'Tuesday: 6am - 6pm';
+  hoursTable.appendChild(hoursTuesday);
+
+  const hoursWednesday = document.createElement('p');
+  hoursWednesday.textContent = 'Wednesday: 6am - 6pm';
+  hoursTable.appendChild(hoursWednesday);
+
+  const hoursThursday = document.createElement('p');
+  hoursThursday.textContent = 'Thursday: 6am - 10pm';
+  hoursTable.appendChild(hoursThursday);
+
+  const hoursFriday = document.createElement('p');
+  hoursFriday.textContent = 'Friday: 6am - 10pm';
+  hoursTable.appendChild(hoursFriday);
+
+  const hoursSaturday = document.createElement('p');
+  hoursSaturday.textContent = 'Saturday: 8am - 10pm';
+  hoursTable.appendChild(hoursSaturday);
+
   mainElement.appendChild(h1Element);
   mainElement.appendChild(presentation);
+  mainElement.appendChild(hoursTable);
   divContent.appendChild(mainElement);
 };
 
