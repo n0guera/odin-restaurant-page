02da7ff83d7563ecbbaa376f7f90ac94924e998b
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-06-19 14:21:55 -0300

    Add IDs to nav tabs

--- src/header.js
+++ src/header.js
@@ -5,12 +5,15 @@ const createHeader = () => {
   const unorderedList = document.createElement('ul');
 
   const listItemOne = document.createElement('li');
+  listItemOne.setAttribute('id', 'home-tab');
   listItemOne.textContent = 'Home';
 
   const listItemTwo = document.createElement('li');
+  listItemTwo.setAttribute('id', 'menu-tab');
   listItemTwo.textContent = 'Menu';
 
   const listItemThree = document.createElement('li');
+  listItemThree.setAttribute('id', 'contact-tab');
   listItemThree.textContent = 'Contact';
 
   unorderedList.appendChild(listItemOne);
