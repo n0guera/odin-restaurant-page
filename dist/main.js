f8308eff5b0a701ae2a504e3fe11b67a8d10ebf2
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-06-22 13:04:29 -0300

    Add contact tab title element

--- dist/main.js
+++ dist/main.js
@@ -6,43 +6,43 @@
     const t = document.createElement('main'),
       n = document.createElement('h1');
     n.textContent = 'An Awesome Restaurant';
-    const d = document.createElement('p');
-    d.textContent =
+    const o = document.createElement('p');
+    o.textContent =
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus qui dolor fugit facere. Corrupti ea sit quidem quod nemo rem aliquid voluptas ratione eos dolorem odio, cum animi! Nemo.';
-    const o = document.createElement('div'),
+    const d = document.createElement('div'),
       c = document.createElement('h2');
-    (c.textContent = 'Hours'), o.appendChild(c);
+    (c.textContent = 'Hours'), d.appendChild(c);
     const a = document.createElement('p');
-    (a.textContent = 'Monday: 6am - 6pm'), o.appendChild(a);
+    (a.textContent = 'Monday: 6am - 6pm'), d.appendChild(a);
     const m = document.createElement('p');
-    (m.textContent = 'Tuesday: 6am - 6pm'), o.appendChild(m);
+    (m.textContent = 'Tuesday: 6am - 6pm'), d.appendChild(m);
     const i = document.createElement('p');
-    (i.textContent = 'Wednesday: 6am - 6pm'), o.appendChild(i);
+    (i.textContent = 'Wednesday: 6am - 6pm'), d.appendChild(i);
     const l = document.createElement('p');
-    (l.textContent = 'Thursday: 6am - 10pm'), o.appendChild(l);
+    (l.textContent = 'Thursday: 6am - 10pm'), d.appendChild(l);
     const r = document.createElement('p');
-    (r.textContent = 'Friday: 6am - 10pm'), o.appendChild(r);
+    (r.textContent = 'Friday: 6am - 10pm'), d.appendChild(r);
     const u = document.createElement('p');
     (u.textContent = 'Saturday: 8am - 10pm'),
-      o.appendChild(u),
+      d.appendChild(u),
       t.appendChild(n),
-      t.appendChild(d),
       t.appendChild(o),
+      t.appendChild(d),
       e.appendChild(t);
   };
   (() => {
     const e = document.createElement('header'),
       t = document.createElement('nav'),
       n = document.createElement('ul'),
-      d = document.createElement('li');
-    d.setAttribute('id', 'home-tab'), (d.textContent = 'Home');
-    const o = document.createElement('li');
-    o.setAttribute('id', 'menu-tab'), (o.textContent = 'Menu');
+      o = document.createElement('li');
+    o.setAttribute('id', 'home-tab'), (o.textContent = 'Home');
+    const d = document.createElement('li');
+    d.setAttribute('id', 'menu-tab'), (d.textContent = 'Menu');
     const c = document.createElement('li');
     c.setAttribute('id', 'contact-tab'),
       (c.textContent = 'Contact'),
-      n.appendChild(d),
       n.appendChild(o),
+      n.appendChild(d),
       n.appendChild(c),
       t.appendChild(n),
       e.appendChild(t),
@@ -66,19 +66,24 @@
       t.setAttribute('id', 'menu-title'), (t.textContent = 'Menu');
       const n = document.createElement('section');
       n.setAttribute('id', 'menu-items'),
-        ((e, t, d) => {
-          const o = document.createElement('article');
-          o.setAttribute('class', 'menu-item');
+        ((e, t, o) => {
+          const d = document.createElement('article');
+          d.setAttribute('class', 'menu-item');
           const c = document.createElement('h2');
-          (c.textContent = 'A normal dish'), o.appendChild(c);
+          (c.textContent = 'A normal dish'), d.appendChild(c);
           const a = document.createElement('p');
           (a.textContent = 'Just a normal dish, nothing special here.'),
-            o.appendChild(a);
+            d.appendChild(a);
           const m = document.createElement('p');
-          (m.textContent = '$3'), o.appendChild(m), n.appendChild(o);
+          (m.textContent = '$3'), d.appendChild(m), n.appendChild(d);
         })(),
         e.appendChild(t),
         e.appendChild(n);
     }),
-    document.querySelector('#contact-tab').addEventListener('click', () => {});
+    document.querySelector('#contact-tab').addEventListener('click', () => {
+      const e = document.querySelector('#content');
+      e.innerHTML = '';
+      const t = document.createElement('h1');
+      (t.textContent = 'Contact Us'), e.appendChild(t);
+    });
 })();
--- src/contact-tab.js
+++ src/contact-tab.js
@@ -1,3 +1,11 @@
-const createContactContent = () => {};
+const createContactContent = () => {
+  const divContent = document.querySelector('#content');
+  divContent.innerHTML = '';
+
+  const contactTitle = document.createElement('h1');
+  contactTitle.textContent = 'Contact Us';
+
+  divContent.appendChild(contactTitle);
+};
 
 export default createContactContent;
