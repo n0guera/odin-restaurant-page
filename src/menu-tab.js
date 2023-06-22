4e06508ba9d2ed4ec30fa6f21c6f8e5c18c39d50
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-06-22 11:13:46 -0300

    Add menu title to menu tab

--- src/menu-tab.js
+++ src/menu-tab.js
@@ -1,3 +1,12 @@
-const createMenuContent = () => {};
+const createMenuContent = () => {
+  const divContent = document.querySelector('#content');
+  divContent.innerHTML = '';
+
+  const menuTitle = document.createElement('h1');
+  menuTitle.setAttribute('id', 'menu-title');
+  menuTitle.textContent = 'Menu';
+
+  divContent.appendChild(menuTitle);
+};
 
 export default createMenuContent;
